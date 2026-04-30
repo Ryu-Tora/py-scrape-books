88948ee58b08970882a5858b2add606cbca06903
Adam Halka <galkaadam4@gmail.com>
2026-04-30 15:48:03 +0200

    Solution

--- books/spiders/example.py
+++ books/spiders/example.py
@@ -1,6 +1,6 @@
 import scrapy
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from books.items import BooksItem
+import re
 
 
 class BooksSpider(scrapy.Spider):
@@ -8,20 +8,11 @@ class BooksSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com/"]
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.driver = webdriver.Chrome()
-
-    def closed(self, reason):
-        self.driver.quit()
-
     def parse(self, response):
         books = response.css("article.product_pod")
 
         for book in books:
-            relative_url = book.css("h3 a::attr(href)").get()
-            url = response.urljoin(relative_url)
-
+            url = response.urljoin(book.css("h3 a::attr(href)").get())
             yield scrapy.Request(url, callback=self.parse_book)
 
         next_page = response.css(".next a::attr(href)").get()
@@ -29,37 +20,15 @@ class BooksSpider(scrapy.Spider):
             yield response.follow(next_page, callback=self.parse)
 
     def parse_book(self, response):
-        self.driver.get(response.url)
+        stock_text = "".join(response.css(".availability::text").getall()).strip()
+        amount_in_stock = int(re.search(r"\d+", stock_text).group())
 
-        title = self.driver.find_element(By.TAG_NAME, "h1").text
-        price = self.driver.find_element(By.CLASS_NAME, "price_color").text
-
-        stock = self.driver.find_element(By.CLASS_NAME, "availability").text
-
-        description = ""
-        try:
-            description = self.driver.find_element(By.ID, "product_description").find_element(
-                By.XPATH, "following-sibling::p"
-            ).text
-        except:
-            pass
-
-        category = self.driver.find_elements(By.CSS_SELECTOR, "ul.breadcrumb li a")[-1].text
-
-        table = self.driver.find_elements(By.CSS_SELECTOR, "table.table-striped tr")
-
-        data = {}
-        for row in table:
-            key = row.find_element(By.TAG_NAME, "th").text
-            value = row.find_element(By.TAG_NAME, "td").text
-            data[key] = value
-
-        yield {
-            "title": title,
-            "price": float(price.replace("£", "")),
-            "amount_in_stock": stock,
-            "rating": response.css("p.star-rating::attr(class)").get().split()[-1],
-            "category": category,
-            "description": description,
-            "upc": data.get("UPC"),
-        }
+        yield BooksItem(
+            title=response.css("h1::text").get(),
+            price=float(response.css(".price_color::text").get().replace("£", "")),
+            amount_in_stock=amount_in_stock,
+            rating=response.css("p.star-rating::attr(class)").get().split()[-1],
+            category=response.css("ul.breadcrumb li a::text").getall()[-1],
+            description=response.css("#product_description ~ p::text").get(),
+            upc=response.xpath("//th[text()='UPC']/following-sibling::td/text()").get(),
+        )
